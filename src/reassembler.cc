925f880c437b77c88c2438301566ee99fb75e6b1
Junwei <1148131355@qq.com>
2024-01-10 14:25:25 -0500

    debug

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -19,6 +19,9 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   
     if(popedSub.index + popedSub.data.length() >= ack_index){
       int endIdx = min(popedSub.data.length(), output.available_capacity() + ack_index - popedSub.index);
+      cout << "available_capacity: " << output.available_capacity() << endl;
+      cout << "ack_index " << ack_index << endl;
+      cout << "endIdx " << endIdx << endl;
       string writedStr = popedSub.data.substr(ack_index - popedSub.index, endIdx);
       output.push(writedStr);
       cout << "push string: " << writedStr << endl;
