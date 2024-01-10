110ca8fec847b3442af11c2f582d346da5f03be2
Junwei <1148131355@qq.com>
2024-01-10 11:47:35 -0500

    fix reassember_cap

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -18,12 +18,13 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
     Sub popedSub = subPriorityQueue.top();
   
     if(popedSub.index + popedSub.data.length() >= ack_index){
-      string writedStr = popedSub.data.substr(ack_index - popedSub.index);
+      int endIdx = min(popedSub.data.length(), output.available_capacity() + ack_index - popedSub.index);
+      string writedStr = popedSub.data.substr(ack_index - popedSub.index, endIdx);
       output.push(writedStr);
       if(popedSub.is_last_substring){
         output.close();
       }
-      ack_index = popedSub.index + popedSub.data.length();
+      ack_index += writedStr.length();
     }
 
     stored_bytes -= popedSub.data.length();
