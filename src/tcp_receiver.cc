4689086b05dada0ff1e1acbd0b58143161d103cc
Junwei <1148131355@qq.com>
2024-01-12 12:29:35 -0500

    tcpreceiver test

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -8,9 +8,10 @@ void TCPReceiver::receive( TCPSenderMessage message, Reassembler& reassembler, W
   if(message.SYN){
     ISN = message.seqno;  // ??
   }
-  Wrap32 payloadSeqno = ISN ? message.seqno + 1 : message.seqno;
-  reassembler.insert(payloadSeqno.unwrap(ISN, checkpoint) - 1, message.payload.string_view(), message.FIN, inbound_stream); // string_view??
-  checkpoint = payloadSeqno.unwrap(ISN, checkpoint) - 1 + message.payload.string_view().length() - 1;
+  Wrap32 payloadSeqno = message.SYN ? message.seqno + 1 : message.seqno;
+  string data = message.payload.release();
+  reassembler.insert(payloadSeqno.unwrap(ISN, checkpoint) - 1, data, message.FIN, inbound_stream); // string_view??
+  checkpoint = payloadSeqno.unwrap(ISN, checkpoint) - 1 + data.length() - 1;
  
 }
 
@@ -19,13 +20,13 @@ TCPReceiverMessage TCPReceiver::send( const Writer& inbound_stream ) const
   // Your code here.
   std::optional<Wrap32> ack;
   if(ISN){
-    ack = Wrap32.wrap(inbound_stream.bytes_pushed() + 1, ISN);
+    ack = Wrap32::wrap(inbound_stream.bytes_pushed() + 1, ISN);
   }
 
 
   TCPReceiverMessage message{
     ack,
-    Writer.available_capacity()
+    inbound_stream.available_capacity()
   };
   
   return message;
