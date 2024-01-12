7e39680dd29494cf5cf7e4c5dd0f504c291ac47e
Junwei <1148131355@qq.com>
2024-01-12 15:11:08 -0500

    lab2 done

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -14,7 +14,7 @@ void TCPReceiver::receive( TCPSenderMessage message, Reassembler& reassembler, W
   uint64_t absSeqno = payloadSeqno.unwrap(ISN, checkpoint);
   string data = message.payload.release();
   reassembler.insert(absSeqno - 1, data, message.FIN, inbound_stream); // string_view??
-  checkpoint = absSeqno - 1 + data.length() - 1;
+  checkpoint = absSeqno - 1 + data.length() - 1; //  index of the last reassembled byte 
  
 }
 
@@ -23,7 +23,8 @@ TCPReceiverMessage TCPReceiver::send( const Writer& inbound_stream ) const
   // Your code here.
   std::optional<Wrap32> ack;
   if(isISNSet){
-    uint64_t ackNo = inbound_stream.bytes_pushed() + 1;
+    uint64_t ackNo = inbound_stream.bytes_pushed() + 1;  // +1: convert stream index to absolute index
+    //  the first (unreceived)byte the receiver is interested in receiving
     ackNo += inbound_stream.is_closed() ? 1 : 0;
     ack = Wrap32::wrap(ackNo, ISN);
   }
