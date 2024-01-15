f6558fc54c36a89993c0a1760fbf01ab42b23e1a
Junwei <1148131355@qq.com>
2024-01-14 23:36:15 -0500

    TCPSender compile

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -127,10 +127,10 @@ void TCPSender::push( Reader& outbound_stream )
   }
   // uint64_t equalWindowSize = max(1UL, static_cast<uint64_t>(window_size));
   uint64_t equalWindowSize =  static_cast<uint64_t>(window_size);
-  uint64_t sendLen = min(outbound_stream.bytes_buffered(), equalWindowSize);
+  uint64_t sendLen = min(outbound_stream.bytes_buffered(), equalWindowSize - SYN);
   sendLen = min(sendLen, TCPConfig::MAX_PAYLOAD_SIZE);
   if(window_size > 0){
-    window_size -= sendLen;
+    window_size -= (sendLen + SYN);
   }
   
   FIN = FIN && window_size > 0;
